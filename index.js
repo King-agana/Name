d0f2083997441fb78a54fed99c83b60b93341e11
King-agana <107753153+King-agana@users.noreply.github.com>
2022-07-05 23:53:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var firstName = 'kingsley'
+var lastName = "Agana"
+var height = "5ft"
+var country = "Nigeria"
+
+name = firstName + ' ' + lastName;
+
+document.write('Name:' + '' + name + '<br/>')
+document.write('Height:' + '' + height + '<br>')
+document.write('Country:' + '' + country + '<br>')
+
+
